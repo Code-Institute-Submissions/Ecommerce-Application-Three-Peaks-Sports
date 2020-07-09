f05debaf653e816fb4968db2ccc1aa1ac08cd166
Eduardo Parkinson <eduardop@tcd.ie>
2020-07-09 15:13:10 +0100

    re-added ignore file

--- Procfile
+++
@@ -1,3 +0,0 @@
-release: python manage.py migrate
-heroku ps:scale web=1
-web: gunicorn ecommerce.wsgi:application
--- cart/contexts.py
+++
@@ -1,9 +0,0 @@
-
-def items_in_cart(request):
-    cart = request.session.get('cart', {})
-    
-    count = 0
-    for product, quantity in cart.items():
-        count += quantity
-    
-    return {'items_in_cart': count}
